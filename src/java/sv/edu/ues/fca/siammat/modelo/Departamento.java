e52b930d0743c4790c582955db494112465fbb2b
galicia <galicia@DESKTOP-Q52NL26>
2017-09-07 00:32:35 -0600

    Guardado de privilegios

---
+++ src/java/sv/edu/ues/fca/siammat/modelo/Departamento.java
@@ -0,0 +1,96 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sv.edu.ues.fca.siammat.modelo;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author galicia
+ */
+@Entity
+@Table(name = "departamento")
+@NamedQueries({
+    @NamedQuery(name = "Departamento.findAll", query = "SELECT d FROM Departamento d")})
+public class Departamento implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @Column(name = "id_departamento")
+    private Integer idDepartamento;
+    @Column(name = "nombre")
+    private String nombre;
+    @OneToMany(mappedBy = "departamento", fetch = FetchType.LAZY)
+    private List<Municipio> municipioList;
+
+    public Departamento() {
+    }
+
+    public Departamento(Integer idDepartamento) {
+        this.idDepartamento = idDepartamento;
+    }
+
+    public Integer getIdDepartamento() {
+        return idDepartamento;
+    }
+
+    public void setIdDepartamento(Integer idDepartamento) {
+        this.idDepartamento = idDepartamento;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public List<Municipio> getMunicipioList() {
+        return municipioList;
+    }
+
+    public void setMunicipioList(List<Municipio> municipioList) {
+        this.municipioList = municipioList;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idDepartamento != null ? idDepartamento.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Departamento)) {
+            return false;
+        }
+        Departamento other = (Departamento) object;
+        if ((this.idDepartamento == null && other.idDepartamento != null) || (this.idDepartamento != null && !this.idDepartamento.equals(other.idDepartamento))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "sv.edu.ues.fca.siammat.modelo.Departamento[ idDepartamento=" + idDepartamento + " ]";
+    }
+    
+}
